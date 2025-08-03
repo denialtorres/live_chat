5b40f3bbfacb59c7d6380b6ef26abccb99071802
Daniel Torres <denial.torres@gmail.com>
2025-08-03 10:40:06 -0600

    Add auto-scroll behavior for new chat messages

--- app/javascript/controllers/conversation_controller.js
+++ app/javascript/controllers/conversation_controller.js
@@ -2,10 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="conversation"
 export default class extends Controller {
-  static targets = ["textArea"]
+  static targets = ["textArea", "messages"]
 
   connect() {
-    this.textAreaTarget.addEventListener("keydown", this.submitForm.bind(this));
+    this.setScrollPosition();
+    this.setupMutationObserver();
+  }
+
+  setupMutationObserver() {
+    // Think of this like having a helper friend who watches the messages box
+    // When new messages appear, the friend tells us "Hey! Something new happened!"
+    this.observer = new MutationObserver(() => {
+      // When our friend sees new messages, we scroll down to see them
+      this.setScrollPosition();
+    });
+
+    // We tell our helper friend what to watch for:
+    this.observer.observe(this.messagesTarget, {
+      childList: true,  // Watch when new message bubbles get added or removed
+      subtree: true     // Also watch inside all the message bubbles for any changes
+    });
+  }
+
+  disconnect() {
+    // When we're done with the chat, we tell our helper friend to stop watching
+    // This is like saying "Thanks for helping! You can go play now!"
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  setScrollPosition() {
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight - this.messagesTarget.clientHeight;
   }
 
   submitForm(event) {
