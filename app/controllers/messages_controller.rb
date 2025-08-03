b8793490e4a12958db8aa8f923422f0dca4c066d
Daniel Torres <denial.torres@gmail.com>
2025-08-03 15:20:07 -0600

    Add message editing functionality
    Adds ability for users to edit their own messages with inline editing
    interface and real-time updates via Turbo Frames and broadcasts.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_conversation
+  before_action :set_message, only: [:edit, :update]
 
   def create
     @message = @conversation.messages.create(message_params)
@@ -7,10 +8,22 @@ class MessagesController < ApplicationController
     redirect_to @conversation
   end
 
+  def edit
+  end
+
+  def update
+    @message.update(message_params)
+    redirect_to @conversation
+  end
+
   private
 
   def set_conversation
     @conversation = Current.user.conversations.find(params.expect(:conversation_id))
+  end
+
+  def set_message
+    @message = @conversation.messages.find(params.expect(:id))
   end
 
   def message_params
